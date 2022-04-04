28f28f5e58b9a1b84c2ff2d0b85cbe94a2c4c7a6
erezviz <erezviz@gmail.com>
2022-04-04 14:43:04 +0300

    added util file

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 console.log('Starting up');
+
+var gProjs = []
+
+function createPloj(name, title) {
+    return {
+        id: name,
+        name,
+        title,
+        desc: '',
+
+
+    }
+}
+
+function createProjs() {
+
+}
