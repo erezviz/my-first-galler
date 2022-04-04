2920a70c753c798756657d07d243052c7f64425e
erezviz <erezviz@gmail.com>
2022-04-04 22:19:46 +0300

    final commit

--- js/proj-controller.js
+++ js/proj-controller.js
@@ -5,58 +5,19 @@ $(document).ready(init)
 
 function init() {
     createProjs()
-    addEventListeners()
     renderPreviews()
-    addDataSetToModal()
-}
-
-
-
-function addEventListeners() {
-    $('a[href*="1"]').click({ modalNum: '1' }, onRenderProjModal)
-    $('a[href*="2"]').click({ modalNum: '2' }, onRenderProjModal)
-}
-
-
-function onRenderProjModal(ev) {
-
-    var modalNum = ev.data.modalNum
-    var currProj = getCurrProj(modalNum)
-    var strHtml = `
-    
-    <div class="modal-body">
-        <!-- Project Details Go Here -->
-        <h2>${currProj.name}</h2>
-        <p class="item-intro text-muted">${currProj.title}</p>
-        <img class="img-fluid d-block mx-auto" src="img/portfolio/${currProj.id}.jpg" alt="">
-        <p></p>
-        <ul class="list-inline">
-            <li>Date: ${currProj.publishedAt}</li>
-            <li>Client: Coding Academy</li>
-            <li><a target="_blank" href="${currProj.url}" class="link-info">Go see the Project</a></li>
-            
-        </ul>
-        <button class="btn btn-primary" data-dismiss="modal" type="button">
-        <i class="fa fa-times"></i>
-         Close Project</button>
-    </div>
-    `
-    console.log(modalNum);
-    $(`.pfm${modalNum}`).html(strHtml)
-
 
 }
 
-function addDataSetToModal() {
-    $('.pfm1').attr('data-modal', 1)
-    $('.pfm2').attr('data-modal', 2)
-}
+
+
+
 
 
 function renderPreviews() {
     var projs = getProjs()
     var strHtmls = projs.map(proj =>
-        `<a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+        `<section onclick="renderModal('${proj.id}')"><a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
         <div class="portfolio-hover">
             <div class="portfolio-hover-content">
                 <i class="fa fa-plus fa-3x"></i>
@@ -67,10 +28,39 @@ function renderPreviews() {
     <div class="portfolio-caption">
         <h4>${proj.name}</h4>
         <p class="text-muted">${proj.title}</p>
-    </div>`
+    </div></section>`
     )
     console.log(projs);
     console.log(strHtmls);
     document.querySelector('.portfolio-item').innerHTML = strHtmls.join('')
 
+}
+
+
+function renderModal(projId) {
+    var proj = getProjById(projId)
+    var strHtml = `
+    <h2>${proj.name}</h2>
+    <p class="item-intro text-muted">${proj.title}</p>
+    <img class="img-fluid d-block mx-auto" src="img/portfolio/${proj.id}.jpg" alt="">
+    <p>${proj.desc}</p>
+    <ul class="list-inline">
+        <li>Date: ${proj.publishedAt}</li>
+        <li>Client: Coding Academy</li>
+        <li><a target="_blank" href="${proj.url}" class="link-info">Go see the Project</a></li>
+    </ul>
+    <button class="btn btn-primary" data-dismiss="modal" type="button">
+<i class="fa fa-times"></i>
+Close Project</button>`
+
+    document.querySelector('.modal-body').innerHTML = strHtml
+}
+
+
+function onContactSubmit() {
+    var txt = document.querySelector('.text-area').value
+    var email = document.querySelector('.email-input').value
+    var subject = document.querySelector('.subject-input').value
+    var url = `https://mail.google.com/mail/?view=cm&fs=1&to=erezviz@gmail.com&su=${subject}&body=${email}, ${txt}`
+    window.open(url)
 }
